1f07091d8e8e3b196481e8139fe0ea8b097bcb27
Nikita <nikitagupta5566@gmail.com>
2018-12-31 18:52:09 +0530

    mongodb-connect

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -12,29 +12,29 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
 
 	const db = client.db('TodoApp');
 
-	// db.collection('Todos').insertOne({
-	// 	text: 'Something to do',
-	// 	completed: false
-	// }, (err,result) => {
-	// 	if (err) {
-	// 		return console.log('Unable to insert todo',err);
-	// 	}
-	// 	console.log(JSON.stringify(result.ops,undefined,2));
-	// });
+	db.collection('Todos').insertOne({
+		text: 'Something to do',
+		completed: false
+	}, (err,result) => {
+		if (err) {
+			return console.log('Unable to insert todo',err);
+		}
+		console.log(JSON.stringify(result.ops,undefined,2));
+	});
 
-	// db.collection('Users').insertOne({
-	// 	name: 'Nikita',
-	// 	age: '19',
-	// 	location: 'sdff',
+	db.collection('Users').insertOne({
+		name: 'Nikita',
+		age: '19',
+		location: 'sdff',
 
-	// }, (err, result) => {
-	// 	if (err) {
-	// 		return console.log("Error");7
-	// 	}
+	}, (err, result) => {
+		if (err) {
+			return console.log("Error");7
+		}
 
-	// 	// console.log(JSON.stringify(result.ops,undefined,2));
-	// 	console.log(JSON.stringify(result.ops[0]._id.getTimestamp()));
-	// });
+		// console.log(JSON.stringify(result.ops,undefined,2));
+		console.log(JSON.stringify(result.ops[0]._id.getTimestamp()));
+	});
 
 	client.close();
 });
